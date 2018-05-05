9cff93520c9b031cf7174686e0f3c7df4113a09c
EvanXzj <2686465170@qq.com>
2018-05-05 12:49:04 +0800

    add a simple server

---
+++ index.js
@@ -0,0 +1,11 @@
+const {createServer} = require('http')
+
+const server = createServer((req,res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello World')
+})
+
+server.listen(3000, () => {
+    console.log('Server running on 3000')
+})
